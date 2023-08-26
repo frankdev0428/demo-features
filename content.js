c42ca70303a60cfc109e5894378f4b493110662b
frankdev0428@gmail.com <frankdev0428@gmail.com>
2023-08-25 21:37:02 -0700

    js loop modify

--- content.js
+++ content.js
@@ -1,9 +1,4 @@
-import React from 'react'
-
-const content = () => {
-  return (
-    <div>content</div>
-  )
+const thisArr = ['computer 1', 'computer 2','computer 3',]
+for ( let i = 0; i < thisArr.length ; i++) {
+    console.log(thisArr[i])
 }
-
-export default content
