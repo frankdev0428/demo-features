cef001c15e45112c64e91b1ecc0dd15258cfc653
frankdev0428@gmail.com <frankdev0428@gmail.com>
2023-08-25 22:40:10 -0700

    modify features

--- content.js
+++ content.js
@@ -1,4 +1,18 @@
-const thisArr = ['computer 1', 'computer 2','computer 3',]
-for ( let i = 0; i < thisArr.length ; i++) {
-    console.log(thisArr[i])
+let sentent =  ['Hello world','this is machien 2','this is machine 3', ]
+let firstCardd = 10;
+let secondCard = 4;
+let blackJack = false ;
+let message = "";
+let cards = ['firstCard','SecondCard','ThirdCard','fourthCard','fifthCard']
+
+function starGame()  {
+    renderGame()
+}
+
+function renderGame() {
+    cardEls.textContent = "Cards" ,
+    // createta a for loop that render otu all cards isntead of just tow
+    for (let i = 0: i < cards.length: i++) {
+       
+    }
 }
